12d3fa85b27b210c0b669e083ddd6c4cf5748d85
quan <quanpham4908@gmail.com>
2024-08-30 23:35:42 +0700

    stateless// statefull

--- src/Example/ChildComponent.jsx
+++ src/Example/ChildComponent.jsx
@@ -1,45 +1,43 @@
 import React, { Fragment } from "react";
-class ChildComponent extends React.Component {
-  state = {
-    firstname: " ",
-    lastname: " ",
-  };
-  handleChangeFirstName = (event) => {
-    this.setState({
-      firstname: event.target.value,
-    });
-  };
-  handleChangeLastName = (event) => {
-    this.setState({
-      lastname: event.target.value,
-    });
-  };
-  handleSubmit = (event) => {
-    // ngan chan refresh lai trang
-    event.preventDefault();
-    console.log(">> check data input: ", this.state);
-  };
-  // re-render
-  render() {
-    console.log(">>> check props: ", this.props);
-    let { name, age, address, arrJobs } = this.props;
-    let a = "";
-    return (
-      <div>
-        <div className="Job-lists">
-          {
-            (a = arrJobs.map((item, index) => {
-              return (
-                <div key={item.id}>
-                  {item.title} - {item.salary}
-                </div>
-              );
-            }))
-          }
-          {console.log(">>> check map array: ", a)}
-        </div>
+// class ChildComponent extends React.Component {
+//   render() {
+//     console.log(">>> check props: ", this.props);
+//     let { name, age, address, arrJobs } = this.props;
+//     let a = "";
+//     return (
+//       <div>
+//         <div className="Job-lists">
+//           {
+//             (a = arrJobs.map((item, index) => {
+//               return (
+//                 <div key={item.id}>
+//                   {item.title} - {item.salary}
+//                 </div>
+//               );
+//             }))
+//           }
+//           {console.log(">>> check map array: ", a)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+const ChildComponent = (props) => {
+  let { arrJobs } = props;
+  let a = "";
+  return (
+    <div>
+      <div className="Job-lists">
+        {arrJobs.map((item, index) => {
+          return (
+            <div key={item.id}>
+              {item.title} - {item.salary}
+            </div>
+          );
+        })}
+        {console.log(">>> check map array: ", a)}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default ChildComponent;
